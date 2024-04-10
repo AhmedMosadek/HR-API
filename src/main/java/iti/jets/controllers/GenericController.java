3776c7801c20d079b3212db2e6975610644fe232
Mosadek <48337574+AhmedMosadek@users.noreply.github.com>
2024-04-10 05:21:49 +0200

    Return Response with statue code

--- src/main/java/iti/jets/controllers/GenericController.java
+++ src/main/java/iti/jets/controllers/GenericController.java
@@ -3,6 +3,8 @@ package iti.jets.controllers;
 import iti.jets.services.BaseService;
 
 import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
+
 import java.util.List;
 
 public abstract class GenericController<T> {
@@ -12,57 +14,68 @@ public abstract class GenericController<T> {
     @POST
     @Consumes("application/json")
     @Produces("text/plain")
-    public String create(T dto) {
+    public Response create(T dto) {
         if (getService().create(dto)) {
-            return dto.getClass().getSimpleName() + " Created";
+            return Response.status(Response.Status.CREATED).entity(dto.getClass().getSimpleName() + " Created").build();
         }
-        return "Error Creating " + dto.getClass().getSimpleName() + ", Please Try Again";
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).
+                entity("Error Creating " + dto.getClass().getSimpleName()
+                        + ", Please Try Again").build();
     }
 
     @GET
     @Path("/{id}")
     @Produces("application/json")
-    public T findById(@PathParam("id") int id) {
-        try {
-            return getService().findById(id);
-        } catch (NullPointerException e) {
-            return null;
+    public Response findById(@PathParam("id") int id) {
+        T result = getService().findById(id);
+        if (result != null) {
+            return Response.ok(result).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
     }
 
     @GET
     @Produces("application/json")
-    public List<T> findAll() {
-        return getService().findAll();
+    public Response findAll() {
+        List<T> results = getService().findAll();
+        if (!results.isEmpty()) {
+            return Response.ok(results).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
     }
 
     @PUT
     @Path("/{id}")
     @Consumes("application/json")
     @Produces("text/plain")
-    public String update(T dto, @PathParam("id") int id) {
+    public Response update(T dto, @PathParam("id") int id) {
         if (getService().update(dto, id)) {
-            return dto.getClass().getSimpleName() + " Updated";
+            return Response.ok(dto.getClass().getSimpleName() + " Updated").build();
+        } else {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error Updating " + dto.getClass().getSimpleName() + ", Please Try Again").build();
         }
-        return "Error Updating " + dto.getClass().getSimpleName() + ", Please Try Again";
     }
 
     @DELETE
     @Path("/{id}")
     @Produces("text/plain")
-    public String delete(@PathParam("id") int id) {
+    public Response delete(@PathParam("id") int id) {
         if (getService().delete(id)) {
-            return "Deleted Successfully";
+            return Response.ok("Deleted Successfully").build();
+        } else {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error Deleting, Please Try Again").build();
         }
-        return "Error Deleting, Please Try Again";
     }
 
     @DELETE
     @Produces("text/plain")
-    public String deleteAll() {
+    public Response deleteAll() {
         if (getService().deleteAll()) {
-            return "All Deleted Successfully";
+            return Response.ok("All Deleted Successfully").build();
+        } else {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error Deleting All, Please Try Again").build();
         }
-        return "Error Deleting All, Please Try Again";
     }
 }
